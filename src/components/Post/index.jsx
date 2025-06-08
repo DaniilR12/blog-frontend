c977eca476a03df3298b7e3d76b588a5a74438ca
Daniil Riazanov <ryazanow45@gmail.com>
2025-06-08 22:55:59 +0200

    1

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -54,7 +54,7 @@ export const Post = ({
       {imageUrl && (
         <img
           className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-          src={`${process.env.RECT_APP_API_URL}${imageUrl}`}
+          src={`https://site-backend-wbcb.onrender.com/${imageUrl}`}
           alt={title}
         />
       )}

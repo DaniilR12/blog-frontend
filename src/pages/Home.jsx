a4c9b7dbc70132f3ce809f3c1f73c66021c3c7aa
Daniil Riazanov <ryazanow45@gmail.com>
2025-06-10 14:37:55 +0200

    sort

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
@@ -13,6 +13,8 @@ export const Home = () => {
   const dispatch = useDispatch()
   const userData = useSelector(state => state.auth.data)
   const { posts, tags } = useSelector(state => state.posts)
+
+  const [active,setActive]= useState(true)
 
   const isPostsLoading = posts.status === 'loading'
   const isTagsLoading = tags.status === 'loading'
@@ -23,12 +25,12 @@ export const Home = () => {
   }, [dispatch])
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
-        <Tab label="Новые" />
-        <Tab label="Популярные" />
+      <Tabs style={{ marginBottom: 15 }} value={active?0:1} aria-label="basic tabs example">
+        <Tab label="Новые" onClick={()=>setActive(true)} />
+        <Tab label="Популярные" onClick={()=>setActive(false)} /> 
       </Tabs>
       <Grid container spacing={4}>
-        <Grid xs={8} item>
+        {active ?(<Grid xs={8} item>
           {(isPostsLoading ? [...Array(5)] : Array.isArray(posts.items) ? posts.items : []).map((value, index) => isPostsLoading ? <Post key={index} isLoading={true} /> : (
             <Post
               id={value._id}
@@ -45,7 +47,25 @@ export const Home = () => {
               isEditable={userData?._id === value.user._id}
             />
           ))}
-        </Grid>
+        </Grid>):(<Grid xs={8} item>
+          {(isPostsLoading ? [...Array(5)] : [...Array.isArray(posts.items) ? posts.items : []].sort((a,b)=>b.viewsCount-a.viewsCount)).map((value, index) => isPostsLoading ? <Post key={index} isLoading={true} /> : (
+            <Post
+              id={value._id}
+              title={value.title}
+              imageUrl={value.imageUrl ? value.imageUrl: ""}
+              user={{
+                avatarUrl: value.user.avatarUrl ? value.user.avatarUrl : 'https://i.pinimg.com/736x/90/f7/a4/90f7a49893bc987858e13e10ffc72a23.jpg',
+                fullName: value.user.fullName,
+              }}
+              createdAt={value.user.createdAt}
+              viewsCount={value.viewsCount}
+              commentsCount={3}
+              tags={value.tags}
+              isEditable={userData?._id === value.user._id}
+            />
+          ))}
+        </Grid>)}
+        
         <Grid xs={4} item>
           <TagsBlock items={Array.isArray(tags.items) ? tags.items : []} isLoading={isTagsLoading} />
           <CommentsBlock
